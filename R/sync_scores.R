b0b8a83f5e1f63a703622a646dbef709f8510761
Carl <cboettig@gmail.com>
2023-01-10 00:47:33 +0000

    sync_scores can live here too

---
+++ R/sync_scores.R
@@ -0,0 +1,15 @@
+
+#' Sync scores from remote server to a local directory
+#' @param dest local directory to cache scores
+#' @export
+sync_scores <- function(dest = "cache/") {
+  
+  minio::install_mc()
+  minio::mc_alias_set("efi",  endpoint="data.ecoforecast.org",
+               access_key = "", secret_key = "")
+  cmd <- paste("mirror --overwrite efi/neon4cast-scores)", dest)
+  minio::mc(cmd)
+
+}
+
+
