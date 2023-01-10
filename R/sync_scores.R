e43ff6b4e4136e5da5dd8f3b5ffcc4019c9da6ff
Carl <cboettig@gmail.com>
2023-01-10 01:18:20 +0000

    typo

--- R/sync_scores.R
+++ R/sync_scores.R
@@ -7,7 +7,7 @@ sync_scores <- function(dest = "cache/") {
   minio::install_mc()
   minio::mc_alias_set("efi",  endpoint="data.ecoforecast.org",
                access_key = "", secret_key = "")
-  cmd <- paste("mirror --overwrite efi/neon4cast-scores)", dest)
+  cmd <- paste("mirror --overwrite efi/neon4cast-scores", dest)
   minio::mc(cmd)
 
 }
